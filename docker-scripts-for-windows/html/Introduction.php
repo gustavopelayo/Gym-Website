85ad9db1fb07ffc4c1d347349d5ace9ac4b17648
Gustavo Pelayo <pelayo208@gmail.com>
2020-12-26 20:26:46 +0000

    Criada main page

--- docker-scripts-for-windows/html/Introduction.php
+++ docker-scripts-for-windows/html/Introduction.php
@@ -1,15 +1,37 @@
 <?php
-   session_start();
-   ?>
-<html>
-<head>
-    <link rel= "stylesheet "href = "style.css" > 
-</head>
+session_start();
+?>
+    <html>
 
-<body>
-    <div class= title>
-        <h1>FitMe</h1>       
-    </div>
-    <span> <?php echo $_SESSION["msg"]; ?> </span> 
-</body>
-</html>
+    <head>
+        <meta charset="utf-8">
+        <title>FitMe</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet " href="main_page_style.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;500;700;900&display=swap" rel="stylesheet">
+    </head>
+
+    <body>
+        <nav>
+            <input type="checkbox" id="check">
+            <label for="check" class="checkbtn">
+            <i class="fa fa-align-justify"></i>
+        </label>
+            <label class="logo">FitMe</label>
+            <ul>
+                <li><a href="#active">Home</a></li>
+                <li><a href="#">Chest</a></li>
+                <li><a href="#">Back</a></li>
+                <li><a href="#">Legs</a></li>
+                <li><a href="#">Arms</a></li>
+                <li><a href="#">Core</a></li>
+            </ul>
+        </nav>
+        <section>
+
+        </section>
+    </body>
+
+    </html>
