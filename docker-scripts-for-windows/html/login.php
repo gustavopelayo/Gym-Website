544fbca230db98fb2be913b578c578250940f3e4
Guilherme Pelayo <guilherme.pelayo@fsstudio.com>
2020-12-27 00:03:23 +0000

    Mudado o index da main page, feita a landing page

--- docker-scripts-for-windows/html/login.php
+++ docker-scripts-for-windows/html/login.php
@@ -68,33 +68,7 @@ unset($_SESSION["msg"]);
             </div>
         </form>
     </div>
-    <!--
-    <div class = "student">
-        <div class = "formbox">
-            <br>
-            <br>
-            <div class= "buttonbox">
-                <button type="button" class="toggle-btn" onclick="login()" >  Login</button>
-                <button type="button" class="toggle-btn" onclick="register()"> Register</button>
-            </div>
-            
-            <form id = "login" class="input-group" action ="action_login.php" method = "post">
-                <input type="text" class = "inputfield" placeholder="userID" name = "username" required>
-                <input type="password" class = "inputfield" placeholder="Enter Password" name = "password" required>
-                <input type="checkbox" class="check-box" > <span>Remember Password</span>
-                <button type = "submit" class="submit-bttn">Login</button>
-                <form> <?php echo $msg; ?> </form> 
-            </form>
-            <form id = "register" class="input-group" method = "post" action = "action_register.php">
-                <input type="text" class = "inputfield" placeholder="userID" name = "username" required>
-                <input type="password" class = "inputfield" placeholder="Enter Password"  name = "password" required>
-                <button type = "submit" class="submit-bttn">Register</button>
-       
-            </form>       
-        </div>       
-    </div>
--->
-
+    
     <script>
         function register() {
             var x = document.getElementById("loginContainer");
